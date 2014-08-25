a58b8f3ef3ab7cab3c31b65a258d73ac7fe6ecd4
labyrinthofdreams <noemail@localhost.localhost>
2014-08-25 18:11:22 +0300

    add copy + move constructors and copy + move assignment operators for templet class

--- templet.hpp
+++ templet.hpp
@@ -124,6 +124,17 @@ public:
      */
     Templet() = default;
 
+    Templet(const Templet& other) : _text(other._text),
+        _parsed(other._parsed.str()),
+        _nodes(other._nodes)
+        {}
+    Templet(Templet&& other) : _text(std::move(other._text)),
+    _parsed(other._parsed.str()),
+    _nodes(std::move(other._nodes))
+    {}
+    Templet& operator=(const Templet&) = default;
+    Templet& operator=(Templet&&) = default;
+
     /**
      * @brief Construct Templet object with template text
      * @param text Template text
